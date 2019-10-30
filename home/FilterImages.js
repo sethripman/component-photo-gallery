db1019258326e2622d93ea640b789c8b0b1a8aac
sethripman <sethripman@gmail.com>
2019-10-30 11:22:48 -0700

    working on dynamically generated options for select

--- home/FilterImages.js
+++ home/FilterImages.js
@@ -4,30 +4,33 @@ class FilterImages extends Component {
 
     onRender(select) {
         const onFilter = this.props.onFilter;
-
+        
         select.addEventListener('input', () => {
             onFilter(select.value);
         });
     }
-
+    
     renderHTML() {
-        return /*html*/`
-            <select class="corn-type-filter">
-                <option value="" selected>All Varieties</option>
-                <option value="dragon">Dragon</option>
-                <option value="lizard">Lizard</option>
-                <option value="chameleon">Chameleon</option>
-                <option value="mouflon">Mouflon</option>
-                <option value="addax">Addax</option>
-                <option value="markhor">Markhor</option>
-                <option value="unicorn">Unicorn</option>
-                <option value="rhino">Rhino</option>
-                <option value="narwhal">Narwhal</option>
-                <option value="triceratops">Triceratops</option>
-                <option value="unilego">Unilego</option>
-            </select>
+        const images = this.props.images;
+        // Using spread operator to take the individual keywords out of a set of unique keywords
+        const uniqueKeywords = [...new Set(images.map(image => image.keyword))];
+
+        
+        let returnHTML = /*html*/`
+        <select class="corn-type-filter">
+        <option value="" selected>All Varieties</option>
+
+            uniqueKeywords.forEach((keyword) => {
+                const option = document.createElement("option")
+                option.value = keyword;
+                option.text = keyword.charAt(0).toUpperCase() + keyword.substring(1);
+            });
+
+        </select>
         `;
+            
+        return returnHTML;
     }
 }
-
+    
 export default FilterImages;
--- non-component-implementation/test/tests.js
+++
@@ -1,2 +0,0 @@
-import './html-equal.js';
-import './render-test.js';
