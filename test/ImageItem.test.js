b6ec5fa4199d8ee515931fe58ed78daac3c24065
sethripman <sethripman@gmail.com>
2019-10-30 09:54:46 -0700

    test working

--- test/ImageItem.test.js
+++ test/ImageItem.test.js
@@ -1,18 +1,18 @@
 import ImageItem from '../home/ImageItem.js';
 const test = QUnit.test;
 
-QUnit.module('Render Cat Item');
+QUnit.module('Render Image Item');
 
 test('renders html from data', assert => {
     // arrange
-    const input = {
+    const image = {
         url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
         title: 'UniWhal',
         description: 'A unicorn and a narwhal nuzzling their horns',
         keyword: 'narwhal',
         horns: 1
     };
-
+    
     const expected = /*html*/`
     <li>
         <div class="info-container">
@@ -28,8 +28,8 @@ test('renders html from data', assert => {
 
     // act
     const props = { image: image };
-    const ImageItem = new ImageItem(props);
-    const html = ImageItem.renderHTML();
+    const newImageItem = new ImageItem(props);
+    const html = newImageItem.renderHTML();
     
     // assert
     assert.htmlEqual(html, expected);
