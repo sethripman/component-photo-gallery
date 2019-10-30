2bf6b1da9a7814667d8679c37aaf70f02ee468e2
sethripman <sethripman@gmail.com>
2019-10-30 11:43:29 -0700

    working on template strings

--- home/FilterImages.js
+++ home/FilterImages.js
@@ -15,16 +15,21 @@ class FilterImages extends Component {
         // Using spread operator to take the individual keywords out of a set of unique keywords
         const uniqueKeywords = [...new Set(images.map(image => image.keyword))];
 
+        const lines = "";
+        uniqueKeywords.forEach((keyword) => {
+            const option = document.createElement("option")
+            option.value = keyword;
+            option.text = keyword.charAt(0).toUpperCase() + keyword.substring(1);
+            lines += option + ;
+        });
+
+        <option value="keyword">Keyword</option>
         
         let returnHTML = /*html*/`
         <select class="corn-type-filter">
-        <option value="" selected>All Varieties</option>
-
-            uniqueKeywords.forEach((keyword) => {
-                const option = document.createElement("option")
-                option.value = keyword;
-                option.text = keyword.charAt(0).toUpperCase() + keyword.substring(1);
-            });
+            <option value="" selected>All Varieties</option>
+            ${`
+                <option></option>${dynamically generated line}`}
 
         </select>
         `;
